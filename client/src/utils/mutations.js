8ffe655e335d0552d0d592ef84d580f29d9cf3db
Keerti Chaudhary <17keerti@gmail.com>
2022-06-03 16:30:19 -0700

    add mutation.js on client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,46 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $book: String!) {
+    saveBook(userId: $userId, book: $book) {
+      _id
+      name
+      book
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      _id
+      name
+      skills
+    }
+  }
+`;
